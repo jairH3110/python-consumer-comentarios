176a9b37060b7b2009c0c90755e886dba81b0d3c
jairH3110 <chiefeon3110@gmail.com>
2023-05-21 01:20:17 -0600

    lol

--- kafka-consumer-mongo.py
+++ kafka-consumer-mongo.py
@@ -39,7 +39,7 @@ except:
     print("Could not connect to MongoDB")
 
 consumer = KafkaConsumer('test',bootstrap_servers=[
-     'localhost:9092'
+     'my-kafkaf.my-kafkaf-headless.kafka-jairh3110.svc.cluster.local:9092'
     ])
 # Parse received data from Kafka
 for msg in consumer:
